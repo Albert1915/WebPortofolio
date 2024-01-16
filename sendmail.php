d8c6b2311130661b313114b1a287479de3ac39aa
Albert Okario <okarioalbert@gmail.com>
2024-01-16 10:39:07 +0700

    sendmail

--- sendmail.php
+++ sendmail.php
@@ -8,41 +8,20 @@
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
 
-    if (isset($_POST['submit'])) {
+    $message = "Pengirim: " . $email ."\r\n"
+                . "Pesan: " .$message;
 
-        $email = $_POST['email']; //Mendapatkan inputan email dari form
-        $message = $_POST['message']; //Mendapatkan inputan nama dari form
+    if( isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $message = $_POST['message'];
 
-        $mail = new PHPMailer(true);
+        die( $email .' '. $message);
 
-        try {
-            //Server settings
-            $mail->isSMTP();
-            $mail->Host       = 'smtp.gmail.com';
-            $mail->SMTPAuth   = true;
-            $mail->Username   = 'albertokario19@gmail.com';
-            $mail->Password   = 'password';
-            $mail->SMTPSecure = 'ssl';
-            $mail->Port       = 465;
-
-            //Recipients
-            $mail->setFrom('albertokario19@gmail.com', 'Pesan dari Form');
-            $mail->addAddress("$email");
-            $mail->addReplyTo('no-reply@gmail.com', 'NO Reply');
-
-            //Content
-            $mail->isHTML(true);
-            $mail->Subject = 'Input dari Form';
-            $mail->Body    = "<h1>Terima Kasih Untuk Tanggapannya</h1> $message";
-            $mail->AltBody = 'Data formulir';
-
-            $mail->send();
-            echo "Data dari Formulir HTML";
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        if( mail($email, $message) ){
+            echo "Email berhasil dikirim";
         }
-    }
-    else {
-        echo "Silakan submit terlebih dahulu";
+        else {
+            echo "Email gagal dikirim";
+        }
     }
 ?>
