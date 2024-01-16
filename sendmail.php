efbb5449022d7ddef577c0eff7b08f6e9cd6ba0e
Albert Okario <okarioalbert@gmail.com>
2024-01-16 10:42:04 +0700

    mail2

--- sendmail.php
+++ sendmail.php
@@ -1,13 +1,4 @@
 <?php
-
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
-
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
-
     $message = "Pengirim: " . $email ."\r\n"
                 . "Pesan: " .$message;
 
