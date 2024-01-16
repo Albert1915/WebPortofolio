08a5bcb347f0be3dcf542166bd2b4b4eb593409e
Albert Okario <okarioalbert@gmail.com>
2024-01-16 14:53:41 +0700

    have fun, sendmail using formspree API

--- sendmail.php
+++ sendmail.php
@@ -1,18 +1,58 @@
 <?php
-    $message = "Pengirim: " . $email ."\r\n"
-                . "Pesan: " .$message;
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    if( isset($_POST['submit'])){
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-        die( $email .' '. $message);
+//Load Composer's autoloader
+// require_once 'C:\xampp\phpMyAdmin\vendor\autoload.php';
 
-        if( mail("okarioalbert@gmail.com", $email, $message) ){
-            echo "Email berhasil dikirim";
-        }
-        else {
-            echo "Email gagal dikirim";
-        }
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+if (isset($_POST['submit'])) {
+
+    $email = $_POST['email']; //Mendapatkan inputan email dari form
+    $message = $_POST['message']; //Mendapatkan inputan nama dari form
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'okarioalbert@gmail.com';
+        $mail->Password   = 'shaman123554';
+        $mail->SMTPSecure = 'tls';
+        // $mail->SMTPAutoTLS = false; 
+        $mail->Port       = 587;
+
+        //Recipients
+        $mail->setFrom("$email", 'Contact Form');
+        $mail->addAddress('okarioalbert@gmail.com');
+        // $mail->addReplyTo('no-reply@gmail.com', 'NO Reply');
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Website Albert';
+        $mail->Body    = "<h1>Content dari Form</h1> $message";
+        $mail->AltBody = 'Data formulir';
+
+        $mail->send();
+        echo "Data dari Formulir HTML";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+}
+else {
+    echo "Silakan submit terlebih dahulu";
+}
+
 ?>
